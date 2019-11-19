58cc05f1ab1512ea4869d968562873a3b12b94a2
kaollie74 <kaollie74@gmail.com>
2019-11-19 17:42:51 -0600

    able to set name to String name using getter and setter methods

--- BankAccoutApp/BankAccountApp2.java
+++ BankAccoutApp/BankAccountApp2.java
@@ -4,7 +4,10 @@ public class BankAccountApp2 {
    public static void main(String[] args) {
     BankAccount2 account1 = new BankAccount2("123456789");
     BankAccount2 account2 = new BankAccount2("123456789");
-    BankAccount2 account3 = new BankAccount2("123456789");
+    BankAccount2 account3BankAccount2 = new BankAccount2("123456789");
+
+    account1.setName("Kyle Allen Olson");
+    System.out.println("On line 10" + account1.getName());
   }
 }
 
@@ -13,24 +16,33 @@ class BankAccount2 {
   //Properties of bank account
   private static int ID = 1000;                       // Internal ID 
   private String accountNumber; // ID + random 2-digit number + first 2 of SSN
-  private String routingNumber;
+  private final static String routingNumber = "98754321"; 
   private String name;
   private String SSN;
   private double balance;
 
+
+
   //Constructors
   public BankAccount2(String SSN) {
-    System.out.println("New Account Created");
+    //System.out.println("New Account Created");
     this.SSN = SSN;
+    System.out.println(SSN);
     ID++;
-    setAccountNumber();
-  
-    
+    setAccountNumber(); 
   }
 
   private void setAccountNumber() {
     int random = (int) (Math.random() * 100);
-    accountNumber = ID + random + SSN.substring(0,1); 
-    System.out.println(accountNumber);
+    accountNumber = ID + "" + random + SSN.substring(0,2); 
+    System.out.println("Your account number: " + accountNumber);
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name; 
   }
 }
