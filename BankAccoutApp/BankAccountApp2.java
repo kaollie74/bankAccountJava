33f6e30edff6f2bc3c66ed32e792fd5ff009e408
kaollie74 <kaollie74@gmail.com>
2019-11-19 18:08:07 -0600

    printed info to new account thre toString method

--- BankAccoutApp/BankAccountApp2.java
+++ BankAccoutApp/BankAccountApp2.java
@@ -1,45 +1,45 @@
 
 public class BankAccountApp2 {
 
-   public static void main(String[] args) {
+  public static void main(String[] args) {
     BankAccount2 account1 = new BankAccount2("123456789", 1000);
     BankAccount2 account2 = new BankAccount2("123456789", 2500);
-    BankAccount2 account3BankAccount2 = new BankAccount2("123456789", 2500);
+    BankAccount2 account3 = new BankAccount2("123456789", 2500);
 
     account1.setName("Kyle Allen Olson");
     System.out.println("On line 10" + account1.getName());
     account1.makeDeposit(100);
     account1.makeDeposit(255);
     account1.payBill(180);
+    account1.accrue();
+    System.out.println(account1.toString());
   }
 }
 
-class BankAccount2 {
-  
-  //Properties of bank account
-  private static int ID = 1000;                       // Internal ID 
+class BankAccount2 implements IInterest {
+
+  // Properties of bank account
+  private static int ID = 1000; // Internal ID
   private String accountNumber; // ID + random 2-digit number + first 2 of SSN
-  private final static String routingNumber = "98754321"; 
+  private final static String routingNumber = "98754321";
   private String name;
   private String SSN;
   private double balance;
 
-
-
-  //Constructors
+  // Constructors
   public BankAccount2(String SSN, double amount) {
-    //System.out.println("New Account Created");
-    balance = amount; 
+    // System.out.println("New Account Created");
+    balance = amount;
     this.SSN = SSN;
     System.out.println(SSN);
     ID++;
-    setAccountNumber(); 
+    setAccountNumber();
   }
 
   private void setAccountNumber() {
 
     int random = (int) (Math.random() * 100);
-    accountNumber = ID + "" + random + SSN.substring(0,2); 
+    accountNumber = ID + "" + random + SSN.substring(0, 2);
     System.out.println("Your account number: " + accountNumber);
   }
 
@@ -48,20 +48,32 @@ class BankAccount2 {
   }
 
   public String getName() {
-    return name; 
+    return name;
   }
 
   public void payBill(double amount) {
     balance -= amount;
-   showBalance();
+    showBalance();
   }
 
   public void makeDeposit(double amount) {
-    balance += amount; 
-   showBalance();
+    balance += amount;
+    showBalance();
   }
 
   public void showBalance() {
     System.out.println(balance);
+  }
+
+  @Override
+  public void accrue() {
+    balance = balance * (1 + rate / 100); // rate comes from interface
+    showBalance();
+  }
+
+  @Override
+  public String toString() {
+    return "[ Name:" + name + "]\n [ Account Number: " + accountNumber + "]\n" + "[Routing Number: " + routingNumber
+        + "]\n" + "[ Balance: " + balance + "]";
   }
 }
